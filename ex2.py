02217e44e3b2fde2a217fa2e60edec78e1cefcad
varshaverse <varshaverse247@gmail.com>
2022-06-15 18:08:07 -0700

    another program

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# a comment, this is so you can read your program later.
+# anything after the # is ignored by python
+
+print("I could have code like this.") # and comment after is ignored
+
+# YOu can also use a comment to "disable" or comment our code:
+# print("This will run.")
+
+print("This will run.")
